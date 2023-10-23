1b0a88d0ad643392cad964f3d06cbd06ccaab348
Muhammad-Shahrukh-Azeem <xazuskhan@gmail.com>
2023-10-23 16:07:48 +0500

    Fixed the chain issue

--- pages/_app.js
+++ pages/_app.js
@@ -4,20 +4,23 @@ import { arbitrum, mainnet, polygon, polygonMumbai } from 'wagmi/chains';
 import { useState, useEffect } from 'react';
 import { Web3Provider } from '@ethersproject/providers';
 import { useWeb3Modal } from '@web3modal/react';
+import { AlchemyProvider } from '@ethersproject/providers';
 import { Web3Modal } from "@web3modal/react";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import '@/styles/globals.css';
 import '@/styles/styles.css';
 
-const chains = [polygon];
-const projectId = '3a9a55fa65c55a359f9fb4a38ac95410';
+// const chains = [polygon];
+const projectId = '5d9d49a124da1cad2eb9597a60dc039c';
 
-const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
+const { chains, publicClient } = configureChains([polygon],[w3mProvider({ projectId })]);
+
 const wagmiConfig = createConfig({
   autoConnect: false,
   connectors: w3mConnectors({ projectId, chains }),
   publicClient,
 });
+
 const ethereumClient = new EthereumClient(wagmiConfig, chains);
 
 function App({ Component, pageProps }) {
@@ -37,11 +40,6 @@ function App({ Component, pageProps }) {
 
   const connectWallet = async () => {
     try {
-      // console.log('Initiating connection...');
-      // console.log('Wagmi Config:', wagmiConfig);
-      // console.log('Ethereum Client:', ethereumClient);
-      // console.log('useWeb3Modal', open);
-  
       try {
         await open();
       } catch (openError) {
@@ -49,36 +47,6 @@ function App({ Component, pageProps }) {
         return;
       }
   
-      // Get the provider from the useWeb3Modal hook
-      // const provider = await ethereumClient.getProvider();
-      // console.log("PROVIDER", provider)
-      // setProvider(provider);
-  
-      // if (provider) {
-      //   const web3ProviderInstance = new Web3Provider(provider);
-      //   setWeb3Provider(web3ProviderInstance);
-  
-      //   const network = await web3ProviderInstance.getNetwork();
-      //   if (network.chainId !== SPECIFIC_CHAIN_ID) {
-      //     try {
-      //       await provider.request({
-      //         method: 'wallet_switchEthereumChain',
-      //         params: [{ chainId: '0x' + SPECIFIC_CHAIN_ID.toString(16) }],
-      //       });
-      //     } catch (switchError) {
-      //       console.error('Error switching to the specific chain:', switchError);
-      //     }
-      //   }
-  
-      //   const signer = web3ProviderInstance.getSigner();
-      //   const address = await signer.getAddress();
-      //   console.log("ADDRESS:", address);
-  
-      //   setSigner(signer);
-      //   setAddress(address);
-      // } else {
-      //   console.error('Provider is undefined after open method call');
-      // }
     } catch (error) {
       console.error('Error connecting wallet:', error);
     }
@@ -104,7 +72,7 @@ function App({ Component, pageProps }) {
       />
     </WagmiConfig>
     
-    <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+    <Web3Modal projectId={projectId} ethereumClient={ethereumClient} defaultChain={polygon} />
     </>
   );
 }
