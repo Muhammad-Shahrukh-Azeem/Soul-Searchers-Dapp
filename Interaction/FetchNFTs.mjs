38a93f495b832b653103c7509867ef502be24afa
Muhammad-Shahrukh-Azeem <xazuskhan@gmail.com>
2023-10-23 12:35:02 +0500

    Modified FetchNFTs.js to get more NFTs

--- Interaction/FetchNFTs.mjs
+++ Interaction/FetchNFTs.mjs
@@ -7,6 +7,8 @@ const settings = {
   apiKey: "_LC-mQ-dmzf-GuyvYVJh0Q_gCM3cbWXR", // Replace with your Alchemy API Key.
   network: Network.MATIC_MAINNET, // Replace with your network.
 };
+const alchemy = new Alchemy(settings);
+
 
 const contractAddress = "0xdcb074190b01a8c08c34866ee972d363c4339d53";
 
@@ -43,5 +45,65 @@ return [NFTS.items, nftsForOwner.totalCount];
 
 }
 
-// const x = await fetchNFTss('0x25dD6D56533F72512BAe2B7a312984241F84De95');
-// console.log(x);
+const x = await fetchNFTss('0xc5C228b086E38e5a084f08715CeF35c68e0871C0');
+console.log(x);
+
+
+
+
+// // Setup: npm install alchemy-sdk
+// import { Alchemy, Network } from "alchemy-sdk";
+
+// // Setup: npm install alchemy-sdk
+
+
+// const config = {
+//   apiKey: "GQmmVecOmQIFVFu4S3HRVsZwlGrOyUf-",
+//   network: Network.ETH_MAINNET,
+// };
+// const alchemy = new Alchemy(config);
+
+// export const getStakedNfts = async () => {
+//   // Wallet address
+//   const address = "0x491bafbe0b3b3d69e4dc7273e7cc8b7b9623eef0";
+
+//   // Initialize variables for pagination
+
+//   let totalCount;
+//   let nftList = [];
+//   let finalList = [];
+//   let pgkey = null;
+//   // Loop through pages until all NFTs are retrieved
+//   do {
+
+//     // console.log("Page key" , pgkey)
+//     // Get NFTs for current page
+//     const nfts = await alchemy.nft.getNftsForOwner(address, { pageKey: pgkey , contractAddresses: ["0x45408ce844d0bf5061e9b25c2924aade4df884b3"]});
+
+//     // Update totalCount
+//     totalCount = nfts.totalCount;
+//     pgkey = nfts.pageKey;
+
+//     // Concatenate current page's NFTs to nftList
+//     nftList = nftList.concat(nfts.ownedNfts);
+//     // Increment page
+
+//   } while (nftList.length < totalCount);
+
+//   // console.log(`Total NFTs owned by ${address}: ${totalCount} \n`);
+
+//   let i = 1;
+
+//   for (let nft of nftList) {
+//     const selectedList = {};
+//     // console.log(`${i}. ${nft.title}, ID:${Number(nft.title.slice(10))}, Image Url:${nft.media[0].raw}`);
+//     selectedList.title = nft.title;
+//     selectedList.value = Number(nft.title.slice(10));
+//     selectedList.tokenURI = nft.media[0].raw;
+//     finalList.push(selectedList);
+//     i++;
+//   }
+
+//   return [finalList, totalCount];
+// };
+
